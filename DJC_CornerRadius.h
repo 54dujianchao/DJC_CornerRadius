d9a6e676d4b39217ed600ea7dfa33f50a0343a5e
杜建超 <dujianchao@dujianchaodeMacBook-Pro.local>
2016-04-15 11:04:37 +0800

    圆角图片

---
+++ DJC_CornerRadius.h
@@ -0,0 +1,15 @@
+//
+//  DJC_CornerRadius.h
+//  DJC_ImageViewDemo
+//
+//  Created by 杜建超 on 15/3/10.
+//  Copyright © 2015年 olinone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (DJC_CornerRadius)
+
+@property (nonatomic, assign) CGFloat aliCornerRadius;
+
+@end
